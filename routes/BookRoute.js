9fbe329ec6851de3aca3c571c8c6716fec1f8bed
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 16:40:29 +0530

    Update Book

--- routes/BookRoute.js
+++ routes/BookRoute.js
@@ -17,32 +17,43 @@ router.get(bookURL, async (req, res) => {
 });
 
 // Create book
-router.post(bookURL,async(req,res) =>{
-    console.log("Book request.....",req.body)
-    try{
-        await bookService.addBook(req.body)
-        return res.status(201).send("Saved Book Successfully")
-
-    }catch(err){
-        console.error(err);
-        res.status(500).send("Internal Server Error")
-
-    }
-})
+router.post(bookURL, async (req, res) => {
+  console.log("Book request.....", req.body);
+  try {
+    await bookService.addBook(req.body);
+    return res.status(201).send("Saved Book Successfully");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 //delete  book
-router.delete(bookURL+"/:id",async(req,res)=>{
-    try{
-        const delBook = await bookService.deleteBook(req.params.id);
-        if(!delBook){
-            return res.status(404).send("Book not found for delete")
-        }
-       return res.status(204).send();
-
-    }catch(err){
-        console.error(err);
-        return res.status(500).send("Internal Server Error")
+router.delete(bookURL + "/:id", async (req, res) => {
+  try {
+    const delBook = await bookService.deleteBook(req.params.id);
+    if (!delBook) {
+      return res.status(404).send("Book not found for delete");
     }
-  })
-  
+    return res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+//update
+router.patch(bookURL + "/:id", async (req, res) => {
+  try {
+    const updatedBook = await bookService.updateBook(req.params.id, req.body);
+    if (!updatedBook) {
+      return res.status(404).send("Book not found");
+    }
+    return res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
