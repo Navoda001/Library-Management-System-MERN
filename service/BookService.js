8e6cd2f80ef41214c21872f54235d61c0794dc58
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 21:03:11 +0530

    fix: add book error

--- service/BookService.js
+++ service/BookService.js
@@ -1,13 +1,24 @@
 //CRUD for Book data handling
 const Book = require('../model/BookModel')
+const {v4: uuidv4} = require('uuid')
 
 async function getAllBooks(){
    return Book.find();
 }
 
 async function addBook(book){
-   const bookData = new Book(book);
-    return bookData.save();
+   const bookData = new Book({
+      bookId: uuidv4(),
+      bookName: book.bookName,
+      author: book.author,
+      edition: book.edition,
+      publisher: book.publisher,
+      isbn: book.isbn,
+      price: parseInt(book.price),
+      totalQty: parseInt(book.totalQty),
+      availableQty: parseInt(book.availableQty),
+    });
+    return bookData.save(bookData);
 }
 
 async function deleteBook(bookId){
