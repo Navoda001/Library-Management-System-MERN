969003a003232148670094e31a2be95e24dd1c39
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 20:38:11 +0530

    update UserService

--- service/UserService.js
+++ service/UserService.js
@@ -1,12 +1,13 @@
 const User = require('../model/UserModel')
 const bcrypt = require('bcryptjs')
+const {v4: uuidv4} = require('uuid')
 
 async function addUser(user){
     try{
         const hashPassword = await bcrypt.hash(user.password, 10)
 
         const newUser = new User({
-            userId: user.userId,
+            userId: uuidv4(),
             firstName: user.firstName,
             lastName: user.lastName,
             email: user.email,
