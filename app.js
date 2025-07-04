2725f1a27f173c27f8f5bcbc804b890f2013dd9d
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 18:38:17 +0530

    add CORS

--- app.js
+++ app.js
@@ -20,10 +20,20 @@ const PORT = 3700
 const bookRoutes = require("./routes/BookRoute")
 const memberRoutes = require("./routes/MemberRoute")
 const mongoose = require("mongoose");
+const cors = require('cors')
 
+//MiddleWares-----------
 app.use(express.json())
-app.use("/api/v1",bookRoutes)
-app.use("/api/v1",memberRoutes)
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ['GET','POST','PATCH','PUT','DELETE','OPTIONS'],
+    allowedHeaders:['Content-Type','Authorization'],
+    credentials: true
+}));
+//----------------------
+
+app.use("/booklib/api/v1",bookRoutes)
+app.use("/booklib/api/v1",memberRoutes)
 
 //DB Integrate
 
