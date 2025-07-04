dad7311cbe2cd477d1c874c1eda3ec583e4740a5
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-07-04 19:19:15 +0530

    Create JWT middleware

---
+++ middleWare/AuthToken.js
@@ -0,0 +1,24 @@
+
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const jwtSecret = process.env.JWT_SECRET
+
+const authToken = (req,res,next) => {
+   const token = req.header('Authorization') && req.header('Authorization').replace('Bearer ','');
+   console.log(token);
+
+   if(!token){
+        return res.staus(401).json({error:"No Token and authorization failed"});
+   }
+   try{
+       console.log("JWT secret",jwtSecret)
+       const decoded = jwt.verify(token,jwtSecret)
+       req.user = decoded
+       next();
+   }catch(err){
+       console.error(err)
+       return res.staus(401).json({error:"Invalid Token"})
+   }
+}
+
+module.exports = authToken;
